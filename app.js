ea9376a702cd2f0a89968c7717abf7738233af3e
Revazi <135771724+ReBojack@users.noreply.github.com>
2023-07-12 03:37:15 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+let videoList = document.querySelectorAll('.video-list-container .list');
+
+videoList.forEach(vid =>{
+   vid.onclick = () =>{
+      videoList.forEach(remove =>{remove.classList.remove('active')});
+      vid.classList.add('active');
+      let src = vid.querySelector('.list-video').src;
+      let title = vid.querySelector('.list-title').innerHTML;
+      document.querySelector('.main-video-container .main-video').src = src;
+      document.querySelector('.main-video-container .main-video').play();
+      document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
+   };
+});
+
+
+const videoThumbnails = document.querySelectorAll(
+  ".video-gallery .all-videos .thumbnail"
+);
+const videoPlayer = document.querySelector(
+  ".video-gallery .featured-video iframe"
+);
+const videoTitle = document.querySelector(".video-gallery .video-title");
+
+const showVideo = (videoId, title) => {
+  let videoUrl = `https://www.youtube.com/embed/${videoId}?rel=0`;
+  videoPlayer.setAttribute("src", videoUrl);
+  videoTitle.innerHTML = title;
+};
+
+videoThumbnails.forEach((v) => {
+  v.addEventListener("click", () => {
+    showVideo(v.dataset.id, v.dataset.title);
+  });
+});
+
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+
+  console.log(Math.floor(window.innerWidth / 270));
+});
+
+// //TOGGLE
+
+// const ball = document.querySelector(".toggle-ball");
+// const items = document.querySelectorAll(
+//   ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+// );
+
+// ball.addEventListener("click", () => {
+//   items.forEach((item) => {
+//     item.classList.toggle("active");
+//   });
+//   ball.classList.toggle("active");
+// });
+
